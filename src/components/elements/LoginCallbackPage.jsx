b3aa550fb62bebd49084f3616bbced5f13192e24
Aryan Goel <aryangoel6900@yahoo.in>
2023-03-24 00:28:44 +0530

    fix: navigate to welcome page on login

--- src/components/elements/LoginCallbackPage.jsx
+++ src/components/elements/LoginCallbackPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {useOktaAuth, LoginCallback} from '@okta/okta-react'
 import {useNavigate} from 'react-router-dom'
+import { WELCOME_ROUTE } from '../constants/routes'
 
 export default function LoginCallbackPage() {
   const {authState} = useOktaAuth()
   const navigate = useNavigate()
-  if (authState?.isAuthenticated) navigate('/home')
+  if (authState?.isAuthenticated) navigate(WELCOME_ROUTE)
   return <LoginCallback />
 }
