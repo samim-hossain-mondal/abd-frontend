da403cffd763df9fdc8d806521972d02fda670b5
Aryan Goel <aryangoel6900@yahoo.in>
2023-03-29 23:37:47 +0530

    fix: make pagination bar fully responsive

--- src/components/elements/welcomePage/PaginatedCards.js
+++ src/components/elements/welcomePage/PaginatedCards.js
@@ -10,7 +10,8 @@ import {
   useMediaQuery,
   IconButton,
   Chip,
-  Box
+  Box,
+  Tooltip,
 } from "@mui/material";
 import propTypes from "prop-types";
 import { OpenInNew, Person } from "@mui/icons-material";
@@ -24,45 +25,53 @@ function ProjectCard({ project, handleProjectClick }) {
       }}
     >
       <CardContent>
-        <Box 
-        sx={{ 
+        <Box
+          sx={{
             // fontSize: isLargeScreen ? 14 : 12,
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
             paddingBottom: 1,
-        }} 
-        color="text.secondary" gutterBottom>
+          }}
+          color="text.secondary"
+          gutterBottom
+        >
           {/* TODO: this is just a dummy implementation */}
-          <Chip 
-            label='IN PROGRESS' 
-            size='small' 
-            variant='outlined' 
+          <Chip
+            label="IN PROGRESS"
+            size="small"
+            variant="outlined"
             color="success"
           />
-          <Box sx={{
-            display: 'flex',
-            alignItems: 'center',
-            }}>
-          <Person />
-          <Typography sx={{fontSize: 12, marginLeft: 1}}>{project._count.projectMembers}</Typography>
-          </Box>
+          <Tooltip title="Number of members">
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Person />
+              <Typography sx={{ fontSize: 13.5, marginLeft: 1 }}>
+                {project._count.projectMembers}
+              </Typography>
+            </Box>
+          </Tooltip>
         </Box>
-        <Typography 
-            variant="h5" 
-            component="div"
-            sx={{
-                fontSize: isLargeScreen ? 20 : 16,
-                textAlign: "start",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                wordBreak: "break-word",
-            }}
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{
+            fontSize: isLargeScreen ? 20 : 16,
+            textAlign: "start",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            wordBreak: "break-word",
+          }}
         >
           {project.projectName}
         </Typography>
-        <Typography 
-        sx={{ 
+        <Typography
+          sx={{
             fontSize: isLargeScreen ? 14 : 12,
             display: "-webkit-box",
             WebkitLineClamp: 1,
@@ -72,56 +81,45 @@ function ProjectCard({ project, handleProjectClick }) {
             overflow: "hidden",
             textOverflow: "ellipsis",
             wordBreak: "break-word",
-        }} 
-        color="text.secondary">
-          {project.projectDescription}
-        </Typography>
-        {/* <Typography
-          variant="body2"
-          sx={{
-            display: "-webkit-box",
-            WebkitLineClamp: 1,
-            WebkitBoxOrient: "vertical",
-            WebkitAlignContent: "start",
-            textAlign: "start",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            wordBreak: "break-word",
           }}
+          color="text.secondary"
         >
           {project.projectDescription}
-        </Typography> */}
+        </Typography>
       </CardContent>
       <CardActions
         sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: 1,
-            paddingTop: 0,
-            flexShrink: 0,
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 1,
+          paddingTop: 0,
+          flexShrink: 0,
         }}
       >
         <Button
-            sx = {{
-                fontSize: isLargeScreen ? 14 : 12,
-                padding: 1,
-                justifyContent: "flex-start",
-                width: isLargeScreen ? 8 : 6,
-            }}
+          sx={{
+            fontSize: isLargeScreen ? 14 : 12,
+            padding: 1,
+            justifyContent: "flex-start",
+            width: isLargeScreen ? 8 : 6,
+          }}
         >
-            VIEW
-            </Button>
-        <IconButton size="small"
-            sx = {{
-                padding: 1
-            }}
+          VIEW
+        </Button>
+        <IconButton
+          size="small"
+          sx={{
+            padding: 1,
+          }}
         >
-          <OpenInNew
-            onClick={(e) => {
-              e.stopPropagation();
-              handleProjectClick(project.projectId);
-            }}
-          />
+          <Tooltip title="Open project dashboard">
+            <OpenInNew
+              onClick={(e) => {
+                e.stopPropagation();
+                handleProjectClick(project.projectId);
+              }}
+            />
+          </Tooltip>
         </IconButton>
       </CardActions>
     </Card>
@@ -131,7 +129,7 @@ function ProjectCard({ project, handleProjectClick }) {
 export default function PaginatedCards({ projects, handleProjectClick }) {
   const isLargeScreen = useMediaQuery("(min-width: 800px)");
   const [page, setPage] = useState(1);
-  const pageSize = isLargeScreen ? 6 : 3;
+  const pageSize = isLargeScreen ? 9 : 3;
 
   const startIndex = (page - 1) * pageSize;
   const endIndex = startIndex + pageSize;
@@ -157,10 +155,20 @@ export default function PaginatedCards({ projects, handleProjectClick }) {
         count={Math.ceil(projects.length / pageSize)}
         page={page}
         onChange={handlePageChange}
-        sx = {{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: 2,
+        size={isLargeScreen ? "large" : "small"}
+        siblingCount={0}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 2,
+          "& .MuiPaginationItem-root": {
+            fontSize: isLargeScreen ? 14 : 12,
+            maxWidth: isLargeScreen ? "unset" : "12px",
+          },
+          "& .Mui-selected": {
+            backgroundColor: "logoBlue.main",
+            color: "white",
+          },
         }}
       />
     </>
