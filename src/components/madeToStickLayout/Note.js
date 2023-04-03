fbd60c3d7961f400b420581f665c30e9850b009b
Abhishek <53441820+Abhishek-Kharpal@users.noreply.github.com>
2023-04-03 21:09:28 +0530

    Feat: CKEditor image (#154)
    * feat: add image integration in made to stick editor
    
    * chore: remove ckfinder
    
    * chore: change pushpinicon to DoneIcon in made to stick
    
    * fix: make scrollbar visible
    
    * chore: fix eslint error
    
    * feat: add token in xmlHTTPRequest
    
    * chore: remove image card from header of made to stick
    
    * chore: replace background color with theme
    
    * fix: theme in made to stick

--- src/components/madeToStickLayout/Note.js
+++ src/components/madeToStickLayout/Note.js
@@ -1,33 +1,145 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable consistent-return */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-multi-assign */
+/* eslint-disable react/no-this-in-sfc */
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
 import proptypes from 'prop-types';
+import { useOktaAuth } from '@okta/okta-react';
 import { TextareaAutosize } from "@mui/base";
 import DeleteIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from "@mui/icons-material/Edit";
-import PushPinRoundedIcon from '@mui/icons-material/PushPinRounded';
+import DoneIcon from '@mui/icons-material/Done';
 import { Box, IconButton, Tooltip } from "@mui/material";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { NOTE_TYPES } from '../constants/MadeToStick';
+import getAccessToken from '../utilityFunctions/getAccessToken';
+import {DOMAIN} from '../../config';
 
 export default function Note({
   card, isEdit, handleCloseButton, handleEditImgLink, handleDelete, handleImageInputChange, handleCardInputChange, handleSave, isPO
 }) {
+  const { authState } = useOktaAuth();
+  const token = getAccessToken(authState);
   const [editButton, setEditButton] = useState(false);
   const [showIcons, setShowIcons] = useState(false);
   const handleEditButton = (value) => {
     setEditButton(value);
-    if(value===false){
+    if (value === false) {
       handleSave();
     }
   };
+  class MyUploadAdapter {
+    constructor(loader) {
+      // The file loader instance to use during the upload.
+      this.loader = loader;
+    }
+
+    // Starts the upload process.
+    upload() {
+      return this.loader.file
+        .then(file => new Promise((resolve, reject) => {
+          this._initRequest();
+          this._initListeners(resolve, reject, file);
+          this._sendRequest(file);
+        }));
+    }
+
+    // Aborts the upload process.
+    abort() {
+      if (this.xhr) {
+        this.xhr.abort();
+      }
+    }
+
+    // Initializes the XMLHttpRequest object using the URL passed to the constructor.
+    _initRequest() {
+      const xhr = this.xhr = new XMLHttpRequest();
+
+      // Note that your request may look different. It is up to you and your editor
+      // integration to choose the right communication channel. This example uses
+      // a POST request with JSON as a data structure but your configuration
+      // could be different.
+      xhr.open('POST',`${DOMAIN}/api/upload`, true);
+      xhr.responseType = 'json';
+      xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+      // setting auth header
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    }
+
+    // Initializes XMLHttpRequest listeners.
+    _initListeners(resolve, reject, file) {
+      const xhr = this.xhr;
+      const loader = this.loader;
+      const genericErrorText = `Couldn't upload file: ${file.name}.`;
+
+      xhr.addEventListener('error', () => reject(genericErrorText));
+      xhr.addEventListener('abort', () => reject());
+      xhr.addEventListener('load', () => {
+        const response = xhr.response;
+
+        // This example assumes the XHR server's "response" object will come with
+        // an "error" which has its own "message" that can be passed to reject()
+        // in the upload promise.
+        //
+        // Your integration may handle upload errors in a different way so make sure
+        // it is done properly. The reject() function must be called when the upload fails.
+        if (!response || response.error) {
+          return reject(response && response.error ? response.error.message : genericErrorText);
+        }
+
+        // If the upload is successful, resolve the upload promise with an object containing
+        // at least the "default" URL, pointing to the image on the server.
+        // This URL will be used to display the image in the content. Learn more in the
+        // UploadAdapter#upload documentation.
+        resolve({
+          default: response.url
+        });
+      });
+
+      // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
+      // properties which are used e.g. to display the upload progress bar in the editor
+      // user interface.
+      if (xhr.upload) {
+        xhr.upload.addEventListener('progress', evt => {
+          if (evt.lengthComputable) {
+            loader.uploadTotal = evt.total;
+            loader.uploaded = evt.loaded;
+          }
+        });
+      }
+    }
+
+    // Prepares the data and sends the request.
+    _sendRequest(file) {
+      // Prepare the form data.
+      const data = new FormData();
+
+      data.append('upload', file);
+
+      // Important note: This is the right place to implement security mechanisms
+      // like authentication and CSRF protection. For instance, you can use
+      // XMLHttpRequest.setRequestHeader() to set the request headers containing
+      // the CSRF token generated earlier by your application.
+
+      // Send the request.
+      this.xhr.send(data);
+    }
+  }
+
+  function uploadPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return new MyUploadAdapter(loader);
+    };
+  }
   return (
     <Box
       className="card"
       sx={{
         backgroundColor: "#EEF2F5",
         overflow: 'scroll',
-        "&::-webkit-scrollbar": { width: 0 },
         "&:hover .edit-Value": { opacity: 1 }
       }}
       style={{
@@ -46,19 +158,19 @@ export default function Note({
           >
             {
               card.type === NOTE_TYPES.IMAGE &&
-                (!isEdit) && (isPO) &&
-                card.value.length !== 0 &&
-                card.value !== "Enter your image url here" && (
-                  <Box
-                    sx={{ fontFamily: "Roboto", padding: '3px' }}
-                  >
-                    <Tooltip title="Edit Image" placement='top'>
-                      <IconButton onClick={() => { handleEditImgLink(card.i) }}>
-                        <EditIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </Box>
-                )
+              (!isEdit) && (isPO) &&
+              card.value.length !== 0 &&
+              card.value !== "Enter your image url here" && (
+                <Box
+                  sx={{ fontFamily: "Roboto", padding: '3px' }}
+                >
+                  <Tooltip title="Edit Image" placement='top'>
+                    <IconButton onClick={() => { handleEditImgLink(card.i) }}>
+                      <EditIcon />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              )
             }
 
             {
@@ -101,7 +213,7 @@ export default function Note({
               >
                 <Tooltip title="Save" placement='top'>
                   <IconButton onClick={handleCloseButton}>
-                    <PushPinRoundedIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} />
+                    <DoneIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} />
                   </IconButton>
                 </Tooltip>
               </Box>
@@ -113,98 +225,100 @@ export default function Note({
         >
           {
             card.type === NOTE_TYPES.TEXT && (
-            <Box sx={{
-              backgroundColor: "#EEF2F5",
-              "&:hover .hover-edit-delete-pin": { opacity: 1 }
-            }}>
-              <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Box 
-                  className="hover-edit-delete-pin" 
-                  sx={{ display: "flex", opacity: 1, flexDirection: "row", flexWrap: "wrap" }}
-                >
+              <Box sx={{
+                backgroundColor: "backgroundColor.secondary",
+                "&:hover .hover-edit-delete-pin": { opacity: 1 }
+              }}>
+                <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <Box
+                    className="hover-edit-delete-pin"
+                    sx={{ display: "flex", opacity: 1, flexDirection: "row", flexWrap: "wrap" }}
+                  >
 
-                  {/* USER only needs to see edit icon if they are not editing the note */}
+                    {/* USER only needs to see edit icon if they are not editing the note */}
+                    {
+                      !editButton && (isPO) && (showIcons) && (
+                        <Tooltip title="Edit" placement='top'>
+                          <IconButton onClick={() => { handleEditButton(true) }} >
+                            <EditIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} />
+                          </IconButton>
+                        </Tooltip>
+                      )
+                    }
+                    {
+                      (isPO) && (showIcons) && (
+                        <Tooltip title="Delete" placement='top'>
+                          <IconButton onClick={() => { handleDelete(card.i) }}>
+                            <DeleteIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} onClick={() => { handleDelete(card.i) }} />
+                          </IconButton>
+                        </Tooltip>
+                      )
+                    }
+                  </Box>
+
+                  {/* USER only needs to see pin icon if they are editing the note. Ii will be ALWAYS visible to remind user to save */}
                   {
-                    !editButton && (isPO) && (showIcons) && (
-                      <Tooltip title="Edit" placement='top'>
-                        <IconButton onClick={() => { handleEditButton(true) }} >
-                          <EditIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} />
-                        </IconButton>
-                      </Tooltip>
+                    editButton && (isPO) && (
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "row",
+                          flexWrap: "wrap",
+                        }}
+                      >
+                        <Tooltip title="Save" placement='top'>
+                          <IconButton onClick={() => { handleEditButton(false) }}>
+                            <DoneIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} />
+                          </IconButton>
+                        </Tooltip>
+                      </Box>
+                    )
+                  }
+
+                </Box>
+                <Box onClick={(e) => e.stopPropagation()}>
+                  {
+                    editButton && (
+                      <CKEditor
+                        editor={ClassicEditor}
+                        config={{
+                          toolbar: [
+                            'heading',
+                            '|',
+                            'bold',
+                            'italic',
+                            'link',
+                            'bulletedList',
+                            'numberedList',
+                            'blockQuote',
+                            'insertTable',
+                            'undo',
+                            'redo',
+                            'uploadImage'
+                          ],
+                          extraPlugins: [uploadPlugin],
+                        }}
+                        onChange={(event, editor) => handleCardInputChange(card.i, editor)}
+                        data={card.value}
+                      />
                     )
                   }
                   {
-                    (isPO) && (showIcons) && (
-                      <Tooltip title="Delete" placement='top'>
-                        <IconButton onClick={() => { handleDelete(card.i) }}>
-                          <DeleteIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} onClick={() => { handleDelete(card.i) }} />
-                        </IconButton>
-                      </Tooltip>
+                    !editButton && (
+                      <CKEditor
+                        disabled
+                        editor={ClassicEditor}
+                        config={{ toolbar: [] }}
+                        onChange={(event, editor) => handleCardInputChange(card.i, event, editor)}
+                        data={card.value}
+                      />
                     )
                   }
                 </Box>
-
-                {/* USER only needs to see pin icon if they are editing the note. Ii will be ALWAYS visible to remind user to save */}
-                {
-                  editButton && (isPO) && (
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "row",
-                        flexWrap: "wrap",
-                      }}
-                    >
-                      <Tooltip title="Save" placement='top'>
-                        <IconButton onClick={() => { handleEditButton(false) }}>
-                          <PushPinRoundedIcon style={{ padding: "3px 3px 0px 3px", cursor: 'pointer' }} />
-                        </IconButton>
-                      </Tooltip>
-                    </Box>
-                  )
-                }
-
+                <style>{`.ck.ck-editor__main>.ck-editor__editable {background-color: #EEF2F5; border: transparent; font-family: Roboto;}`}</style>
+                <style>{`.ck-rounded-corners .ck.ck-editor__top .ck-sticky-panel .ck-toolbar, .ck.ck-editor__top .ck-sticky-panel .ck-toolbar.ck-rounded-corners { border: none`}</style>
               </Box>
-              <Box onClick={(e) => e.stopPropagation()}>
-                {
-                  editButton && (
-                    <CKEditor
-                      editor={ClassicEditor}
-                      config={{
-                        toolbar: [
-                          'heading',
-                          '|',
-                          'bold',
-                          'italic',
-                          'link',
-                          'bulletedList',
-                          'numberedList',
-                          'blockQuote',
-                          'insertTable',
-                          'undo',
-                          'redo',
-                        ]
-                      }}
-                      onChange={(event, editor) => handleCardInputChange(card.i, editor)}
-                      data={card.value}
-                    />
-                  )
-                }
-                {
-                  !editButton && (
-                    <CKEditor
-                      disabled
-                      editor={ClassicEditor}
-                      config={{ toolbar: [] }}
-                      onChange={(event, editor) => handleCardInputChange(card.i, event, editor)}
-                      data={card.value}
-                    />
-                  )
-                }
-              </Box>
-              <style>{`.ck.ck-editor__main>.ck-editor__editable {background-color: #EEF2F5; border: transparent; font-family: Roboto;}`}</style>
-              <style>{`.ck-rounded-corners .ck.ck-editor__top .ck-sticky-panel .ck-toolbar, .ck.ck-editor__top .ck-sticky-panel .ck-toolbar.ck-rounded-corners { border: none`}</style>
-            </Box>
-          )}
+            )}
           {card.type === NOTE_TYPES.IMAGE && isEdit === card.i && (
             <TextareaAutosize
               style={{ width: "95%", overflow: "hidden", opacity: 0.5, fontFamily: "Roboto", border: "1px solid black", fontSize: "large", padding: "10px" }}
